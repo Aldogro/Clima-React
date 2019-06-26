57edde8158ea1b6fad9eac569dcb8b82d6d58e93
Aldogro <49069343+Aldogro@users.noreply.github.com>
2019-06-26 06:46:53 -0300

    build folder

---
+++ build/precache-manifest.8d398c86b7a1e023ad276817d81e8d9c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f83845cc2f652e270bd6db432e863b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1b41a80fb14fbedbcd4",
+    "url": "./static/css/main.2adc67af.chunk.css"
+  },
+  {
+    "revision": "45e50d0479358bfadb7d",
+    "url": "./static/js/2.def297ae.chunk.js"
+  },
+  {
+    "revision": "c1b41a80fb14fbedbcd4",
+    "url": "./static/js/main.9a874dd2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "28326381da4365c0c0726f9eaa5fcf24",
+    "url": "./static/media/01-sunny.28326381.svg"
+  },
+  {
+    "revision": "f0de1c91c75567dfcfd9da22bf0f9703",
+    "url": "./static/media/02-parciallysunny.f0de1c91.svg"
+  },
+  {
+    "revision": "295786be7c872875a17f84ba24416347",
+    "url": "./static/media/0304-cloudy.295786be.svg"
+  },
+  {
+    "revision": "109bab6de27e787f846f4c8a87cc13a0",
+    "url": "./static/media/09-veryrainy.109bab6d.svg"
+  },
+  {
+    "revision": "cb8685e23451467977445e9cf8ff8f4b",
+    "url": "./static/media/10-rainy.cb8685e2.svg"
+  },
+  {
+    "revision": "99bb1ef93ce134ee06e17d088bc4b53d",
+    "url": "./static/media/11-electricstorm.99bb1ef9.svg"
+  },
+  {
+    "revision": "5b94eb751deddc74b24eefb8b5d8c907",
+    "url": "./static/media/13-snow.5b94eb75.svg"
+  },
+  {
+    "revision": "0d56e06d0de2ad050656cf9f6e207239",
+    "url": "./static/media/50-fog.0d56e06d.svg"
+  },
+  {
+    "revision": "0eff60f26eb30c7c70d94affd2c11548",
+    "url": "./static/media/aguja.0eff60f2.svg"
+  },
+  {
+    "revision": "c26dfab15bdcc7176a6b79e24cb0a299",
+    "url": "./static/media/brujula.c26dfab1.svg"
+  },
+  {
+    "revision": "91ae921e2e6ee4550da9d1b704baa759",
+    "url": "./static/media/sinIcono.91ae921e.svg"
+  }
+]);
